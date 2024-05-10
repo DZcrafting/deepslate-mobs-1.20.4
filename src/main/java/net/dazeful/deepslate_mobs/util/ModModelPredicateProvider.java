27e5de1530a93ba820ad94d9ee3a1660183e4208
DZcrafting <david@24net.dk>
2024-05-10 16:50:06 +0200

    Long Bow draw fix. Damage doesnt work

--- src/main/java/net/dazeful/deepslate_mobs/util/ModModelPredicateProvider.java
+++ src/main/java/net/dazeful/deepslate_mobs/util/ModModelPredicateProvider.java
@@ -18,7 +18,7 @@ public class ModModelPredicateProvider {
             if (entity.getActiveItem() != stack) {
                 return 0.0f;
             }
-            return (float)(stack.getMaxUseTime() - entity.getItemUseTimeLeft()) / 20.0f;
+            return (float)(stack.getMaxUseTime() - entity.getItemUseTimeLeft()) / 40.0f;
         });
         ModelPredicateProviderRegistry.register(bow, new Identifier("pulling"),
                 (stack, world, entity, seed) -> entity != null && entity.isUsingItem() && entity.getActiveItem() == stack ? 1.0f : 0.0f);
